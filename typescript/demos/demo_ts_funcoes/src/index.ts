20537d4f50c9ac7628192a9efeefd8e06367798b
Júlio Machado <julioapm@gmail.com>
2023-11-01 14:39:26 -0300

    demo funções atualização

--- typescript/demos/demo_ts_funcoes/src/index.ts
+++ typescript/demos/demo_ts_funcoes/src/index.ts
@@ -19,3 +19,31 @@ console.log(saudacao);
 const saudacao2 = criarSaudacao();
 const saudacao3 = criarSaudacaoDefault();
 console.log(saudacao2);
+
+
+const anonima = function (nome:string) {
+    return `Alô, ${nome}!`;
+};
+console.log(anonima);
+console.log(anonima('teste'));
+
+const saudacaoArrow = (nome:string) => `Alô, ${nome}!`;
+console.log(saudacaoArrow);
+console.log(saudacaoArrow('teste2'));
+
+//Execução imediata de funções
+(function (x:number, y:number) {
+    console.log(x+y);
+})(1,2);
+
+//Closure
+function multiplicar(fator:number): (n:number) => number {
+    return numero => numero * fator;
+}
+const mult2 = multiplicar(2);
+const mult10 = multiplicar(10);
+console.log(mult2(3));
+let n = 5;
+const multn = multiplicar(n);
+n = 7;
+console.log(multn(2));
